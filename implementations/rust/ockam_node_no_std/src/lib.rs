a6a82095e962fba067e0f29dfc09859951b78e37
Mrinal Wadhwa <mrinal@ockam.io>
2021-01-13 15:27:37 -0800

    feat(rust): add no_std for example

--- implementations/rust/ockam_node_no_std/src/lib.rs
+++ implementations/rust/ockam_node_no_std/src/lib.rs
@@ -1,9 +1,10 @@
+#![no_std]
+
 use core::future::Future;
 
 pub fn block_on<T>(future: impl Future<Output = T> + 'static + Send) -> T
 where
     T: Send + 'static,
 {
-    println!("EXECUTOR");
     executor::block_on(future)
 }
